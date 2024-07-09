4da586aefdd3ed2e620538cb30c397e2656cd0d5
Avijit <contact.roy17@gmail.com>
2024-07-10 01:41:45 +0200

    Event handling added.

--- src/components/VueWorld.vue
+++ src/components/VueWorld.vue
@@ -10,9 +10,16 @@
   <h3>Phone: {{ address().phone }}</h3>
   <hr />
   <div id="main">
-    <div id="left">left</div>
-    <div id="right">right</div>
+    <div id="left">Name: {{ getName("Avijit Roy") }}</div>
+    <div id="right">
+      <p>Phone number: {{ getPhoneNumber(12345) }}</p>
+      <p>Message: {{ getMessage() }}</p>
+    </div>
   </div>
+  <hr />
+  <button v-on:click="clickTest()">Click me</button>
+  <button v-on:dblclick="doubleClickTest()">Click me</button>
+  <h2>{{ count }}</h2>
 </template>
 <script>
 export default {
@@ -20,6 +27,7 @@ export default {
   data() {
     return {
       message: "This is a framework based on JavaScript",
+      count: 0,
       usage: function () {
         return "Frontend development for complex web applications";
       },
@@ -35,10 +43,32 @@ export default {
       },
     };
   },
+  methods: {
+    getName(name) {
+      return name;
+    },
+    getPhoneNumber(phone) {
+      return phone;
+    },
+    getMessage() {
+      return this.message;
+    },
+    clickTest() {
+      return this.count++;
+    },
+    doubleClickTest() {
+      return alert("Button is double clicked");
+    },
+  },
 };
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 h3 {
   color: lightseagreen;
 }
@@ -58,8 +88,17 @@ h3 {
 #right {
   width: 50%;
   height: 300px;
-  line-height: 300px;
   text-align: center;
   background-color: crimson;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#right p {
+  margin: 0;
+  line-height: normal;
+  padding: 10px;
+  color: white;
 }
 </style>
