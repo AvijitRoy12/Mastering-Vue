7b96bab916967cdfe0500c0272337308faf66943
Avijit <contact.roy17@gmail.com>
2024-07-10 00:04:42 +0200

    Interpolation added.

---
+++ src/components/VueWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>This is vue world</h1>
+  <h1>My name is {{ name }}</h1>
+</template>
+<script>
+export default {
+  name: "VueWorld",
+  data() {
+    return {
+      name: "Avijit",
+    };
+  },
+};
+</script>
