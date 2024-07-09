e12f9c743b79a0d2d28a071fa80c868124b039fd
Avijit <contact.roy17@gmail.com>
2024-07-10 00:45:04 +0200

    Styling added.

--- src/components/VueWorld.vue
+++ src/components/VueWorld.vue
@@ -8,6 +8,11 @@
   <h3>Name: {{ address().name }}</h3>
   <h3>Address: {{ address().address }}</h3>
   <h3>Phone: {{ address().phone }}</h3>
+  <hr />
+  <div id="main">
+    <div id="left">left</div>
+    <div id="right">right</div>
+  </div>
 </template>
 <script>
 export default {
@@ -32,3 +37,29 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+h3 {
+  color: lightseagreen;
+}
+#main {
+  width: 100%;
+  height: 300px;
+  position: relative;
+  display: flex;
+}
+#left {
+  width: 50%;
+  height: 300px;
+  line-height: 300px;
+  text-align: center;
+  background-color: antiquewhite;
+}
+#right {
+  width: 50%;
+  height: 300px;
+  line-height: 300px;
+  text-align: center;
+  background-color: crimson;
+}
+</style>
